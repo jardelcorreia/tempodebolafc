197dacc8dd96c0c4a3938fc5fa1c07f7bbd3b2da
Jardel <125412133+jardelcorreia@users.noreply.github.com>
2025-07-30 09:08:06 -0300

    Update news.ts

--- src/lib/news.ts
+++ src/lib/news.ts
@@ -42,7 +42,7 @@ export async function getNews(category: 'brasil' | 'internacional' | 'mercado' =
       default:
         params = new URLSearchParams({
           resultType: 'articles',
-          keyword: brasileirão OR "série a" OR "copa do brasil" OR "futebol brasileiro" OR flamengo OR palmeiras OR corinthians',
+          keyword: 'brasileirão OR "série a" OR "copa do brasil" OR "futebol brasileiro" OR flamengo OR palmeiras OR corinthians',
           keywordLoc: 'title,body',
           lang: 'por',
           sourceLocationUri: 'http://en.wikipedia.org/wiki/Brazil',
